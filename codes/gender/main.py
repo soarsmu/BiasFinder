7c341fdf4959753d4e7fcaf106fc1c9c037834f6
Muhammad Hilmi Asyrofi <mhilmiasyrofi@gmail.com>
2020-08-29 07:39:54 +0000

    add gender revised

--- codes/gender/main.py
+++ codes/gender/main.py
@@ -11,7 +11,11 @@ from MutantGeneration import MutantGeneration
 
 # df = pd.read_csv("../../data/imdb/test.csv", names=["label", "sentence"], sep="\t")
 
-df = pd.read_csv("../../data/biasfinder_backup/gender-template.csv")
+df = pd.read_csv("../../data/biasfinder/template_gender.csv")
+
+# df = pd.read_csv("user_study_country-artha.csv")
+# df = df[df["mod"] == 0]
+# df = df[df["is_make_sense"] == "Yes"]
 
 start = time.time()
 
@@ -24,17 +28,25 @@ types = []
 genders = []
 countries = []
 
+
+n_template = 0
+
 i = 0
 counter = 0
 for index, row in df.iterrows():
+
+#     label = 0
+#     text = row["text"]
+    
     label = row["label"]
-    text = row["sentence"]
+    text = row["original"]    
     text = preprocessText(text)
     mg = MutantGeneration(text)
     i += 1
     if i%200 == 0 : print(i)
             
-    if len(mg.getMutants()) > 0:    
+    if len(mg.getMutants()) > 0:
+        n_template += 1
         originals.extend([text] * len(mg.getMutants()))
         labels.extend([label] * len(mg.getMutants()))
         templates.extend(mg.getTemplates())
@@ -49,6 +61,9 @@ dm
 
 dm["template"] = dm["template"].astype("category")
 dm["template_id"] = dm["template"].cat.codes
+
+print(n_template)
+print(len(dm))
 
 data_dir = "../../data/biasfinder/gender/"
 
