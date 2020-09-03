1a5ba8de1c0836382fcd5071ecd2b8c96a2955bb
imamnurby <imamnurby@users.noreply.github.com>
2020-09-03 12:21:32 +0800

    add pipeline to porcess movie review

--- codes/occupation/new copy.py
+++ codes/occupation/new copy.py
@@ -1,19 +1,22 @@
-# df = pd.read_csv("./new/check.csv")
-# # df = df.sample(n = 1000, random_state = 123)
-# excludeList = ['director', 'actor', 'actress', 'filmmaker', 'writer', 'artist', 'scriptwriter', 'reviewer', 'commentator', 'slasher']
-
-symbol = ["!","#","$","%","*",",","/",":",";","@","^","_","`","|","~", ".", "?", ")", ">", "~"]
-auxil = ["'m", "'s","'d","'ll","'ve","n't","'re"]
-
 import pandas as pd
 import spacy
 import re
+import time
+import inflect
 from spacy.matcher import Matcher
 from pycorenlp import StanfordCoreNLP
-
+symbol = ["!","#","$","%","*",",","/",":",";","@","^","_","`","|","~", ".", "?", ")", ">", "~"]
+auxil = ["'m", "'s","'d","'ll","'ve","n't","'re"]
 nlp_wrapper = StanfordCoreNLP('http://localhost:9000')
 nlp = spacy.load('en_core_web_sm')
 matcher = Matcher(nlp.vocab)
+p = inflect.engine()
+
+### Class Definition
+#
+#
+#
+#
 
 class annotatedText:
     # Class to represent annotated text resulted from StandfordCoreNLP
@@ -95,7 +98,6 @@ class annotatedText:
             text = text + ' ' + self.getSentence(i)
         return text.strip() 
     
-
 class annotatedTextOcc(annotatedText):
     def getName(self, sentenceIndex):
         # input:
@@ -189,6 +191,12 @@ class annotatedTextOcc(annotatedText):
             
         return occupationDict
 
+### Helper Function to Generate Mutant
+#
+#
+#
+#
+
 def generateMutantText(at):
     # input:
         # at -> annotatedText object
@@ -204,10 +212,12 @@ def generateMutantText(at):
         for occurance in occurancesList:
             sentenceIndex, tokenBegin, tokenEnd = occurance
             mutantSentence, checkedSpan = mutantGenerator(at, key, tokenBegin, tokenEnd, sentenceIndex, checkedSpan)
-            if sentenceIndex not in tempMutantDict.keys():
-                tempMutantDict[sentenceIndex] = mutantSentence
-        mutantText = rearrangeSentence(tempMutantDict, at)   
-        templateList.append(mutantText)
+            if mutantSentence != '':
+                if sentenceIndex not in tempMutantDict.keys():
+                    tempMutantDict[sentenceIndex] = mutantSentence
+        if len(tempMutantDict) > 0:
+            mutantText = rearrangeSentence(tempMutantDict, at)   
+            templateList.append(mutantText)
     return templateList
 
 def mutantGenerator(at, occupation, tokenBegin, tokenEnd, sentenceIndex, checkedSpan):
@@ -223,7 +233,6 @@ def mutantGenerator(at, occupation, tokenBegin, tokenEnd, sentenceIndex, checked
     occupationSpan, checkedSpan = getOccupationSpan(spacyDoc, occupation, checkedSpan)
     mutantSentence = generateMutantSentence(spacyDoc, occupationSpan)
     mutantSentence = re.sub(r'\b' + occupation + r'\b', '<OCC>', mutantSentence)
-    print(mutantSentence)
     return mutantSentence.strip(), checkedSpan
 
 def getOccupationSpan(spacyDoc, occupation, checkedSpan):
@@ -254,23 +263,25 @@ def generateMutantSentence(spacyDoc, occupationSpan):
         # occupationSpan -> spacy span
     # output:
         # sentenceAfterDeleteAdj -> string
-    occupationToken = occupationSpan.root
-    sentenceAfterDelAdj = None
-    
-    tempDict = {}
-    tempDict[occupationToken] = occupationToken.i
-    for token in occupationToken.subtree:
-        tempDict[token] = token.i
+    sentenceAfterDelAdj = ''
+    if occupationSpan != None:
+        occupationToken = occupationSpan.root
         
-    beginSpanStopIndex = min(tempDict.values())
-    endSpanStartIndex = occupationToken.i + 1
-    
-    beginSpan = spacyDoc[0:beginSpanStopIndex].text
-    endSpan = spacyDoc[endSpanStartIndex:].text
-    
-    occString = generatePlaceholder(occupationToken)
-    
-    sentenceAfterDelAdj = '{} {} {}'.format(beginSpan,occString,endSpan).strip()
+        
+        tempDict = {}
+        tempDict[occupationToken] = occupationToken.i
+        for token in occupationToken.subtree:
+            tempDict[token] = token.i
+            
+        beginSpanStopIndex = min(tempDict.values())
+        endSpanStartIndex = occupationToken.i + 1
+        
+        beginSpan = spacyDoc[0:beginSpanStopIndex].text
+        endSpan = spacyDoc[endSpanStartIndex:].text
+        
+        occString = generatePlaceholder(occupationToken)
+        
+        sentenceAfterDelAdj = '{} {} {}'.format(beginSpan,occString,endSpan).strip()
     return sentenceAfterDelAdj
 
 def generatePlaceholder(occupationToken):
@@ -282,7 +293,7 @@ def generatePlaceholder(occupationToken):
     repairedOcc = ''
     for token in occupationToken.children:
         if token.pos_  == 'DET' and token.i < occupationToken.i:
-            if token.i == (occupationToken.i-1) and token.text.lower() in ['an', 'a']:
+            if token.text.lower() in ['an', 'a']:
                 repairedOcc = repairedOcc + ' ' + ''.join("<DET>")
             else:
                 repairedOcc = repairedOcc + ' ' + ''.join(token.text)
@@ -303,73 +314,34 @@ def rearrangeSentence(mutantDict, at):
 test = "John is a  hard-worker excellent fisherman and he meet with other fisherman today. However, his wife, a teacher, goes to school. John meets with other fisherman today"
 at = annotatedTextOcc(test)
 
-    # def getTokenEndSentence(self, sentenceIndex):
-    #     if sentenceIndex != self.getNumberOfSentence():
-    #         # print(self.getNumberOfSentence())
-    #         # print(self.getAnnotatedSentence(sentenceIndex))
-    #         numberOfToken = (self.getNumberOfToken(sentenceIndex))
-    #         # print(numberOfToken)
-    #         endToken = self.getAnnotatedSentence(sentenceIndex)['tokens'][numberOfToken-1]['characterOffsetEnd']
-    #         beginToken = self.getAnnotatedSentence(sentenceIndex+1)['tokens'][numberOfToken]['characterOffsetEnd']
-    #         print(self.getOriginalText())
-    #         print(self.getOriginalText()[0:endToken])
-    #         print(self.getOriginalText()[beginToken:])
-        
-# import time
-# start_time = time.time()
+### Pipeline to Process Movie Review
+#
+#
+#
+#
 
-# counter = 1
-# templateList1 = []
-# for index, row in df.iterrows():
-#     print("counter: {}".format(counter))
-#     counter += 1
-#     oriText = row.original
-#     # oriText = "There must have been a sale on this storyline back in the 40's. An epidemic threatens New York ( it's always New York) and nobody takes it seriously. Some might say that Richard Widmark and Jack Palance did it better in Panic in the Streets, but I disagree. There is always something about these Poverty Row productions that really touch a nerve. The production values are never that polished and the acting is a little rough around the edges, but that is the very reason I think this movie and those like it are effective. Rough, grainy, edgy. And the cast. All 2nd stringers or A list actors past their prime. No egos here. These folks were happy to get the work. Whit Bissell, Carl Benton Reid, Jim Backus, Arthur Space, Charles Korvin, and the melodious voice of Reed Hadley flowing in the background like crude oil. By the way, I've been in the hospital a couple of times; how come my nurses never looked like Dorothy Malone? In these kind of movies they don't bother much with make - up and hair, but they really managed to turn Evelyn Keyes into a hag. Or maybe they just skipped the make - up and hair altogether. Anyway, it was pretty effective. She plays a lovesick jewel smuggler who picks up a case of Small Pox in Cuba while smuggling jewels back for ultra - villain Charles Korvin ( who is boffing her sister in the meantime). You got the Customs Agents looking for her because of the jewels, and the Health Department looking for her because she's about to de - populate New York. No 4th Amendment rights here. Everybody gets hassled. You got ta have the right attitude to enjoy a movie like this. I have a brother who scrutinizes movies to death. If they don't hold up to his Orson Wellian standards, he bombs them unmercifully. They must have the directorial excellence of a David Lean movie, the score of Wolfgang von Korngold, the Sound and Art of Douglas Shearer and Cedric Gibbons respectively. This ain't it. But I have the right attitude, and if you do as well, you'll love this movie."
-#     at = annotatedTextOcc(oriText)
-#     occList = []
-#     for i in range(at.getNumberOfSentence()):
-        
-#         occTempList = at.getOccupation(i)
-        
-#         occList += occTempList
-    
-#     print(occList)
-#     if len(occList) == 1 and occList[0] not in excludeList:
-#         templateList1.append((row.template_id,row.template))
-        
-            
-    
+df = pd.read_csv("./asset/test.csv", encoding="latin1", sep="\t", names=["sentiment", "review"])
+df = df.sample(n = 100, random_state = 123)
+occPlaceholder = pd.read_csv("./asset/neutral-occupation.csv", names=['occupation'])
+occPlaceholderList = occPlaceholder['occupation'].to_list()
 
+start = time.time()
+mutantTextList = []
+counter = 1
+for index, row in df.iterrows():
+    print(f"count: {counter}")
+    counter += 1
+    at = annotatedTextOcc(row.review)
+    templateList = generateMutantText(at)
+    if len(templateList) > 0:
+        for template in templateList:
+            for occ in occPlaceholderList:
+                mutantText = template.replace("<DET> <OCC>", p.a(occ)).replace("<OCC>", occ)
+                mutantTextList.append((row.sentiment, mutantText, mutantText, template, row.review, occ))
 
-# print("--- %s seconds ---" % (time.time() - start_time))
+if len(mutantTextList) > 0:
+    outputDataOcc = pd.DataFrame(mutantTextList)
+    outputDataOcc.columns = ['0', '1', 'mutant', 'template', 'original', 'occupation']
+    outputDataOcc.to_csv("./result/result.csv", index=False)
 
-# placeholderList = pd.read_csv("./asset/neutral-occupation.csv")
-# placeholderList = placeholderList['occupation'].to_list()
-# import inflect
-# mutantList = []
-# p = inflect.engine()
-# for t in templateList1:
-#     template, label, ori = t
-#     for element in placeholderList:
-#         mutant = ''.join(template.replace('@DetAAN', (p.a(element).split()[0])).replace('@OCCUPATION', element).replace('@CorefPronounHER', 'her').replace('@CorefPronounHIS', 'his').replace('@CorefPronounSHE', 'she').replace('@CorefPronounHE', 'he').replace("@CorefOCCUPATION", element))
-#         mutantList.append((label, mutant, mutant, template, ori, element))
-
-
-
-        # if at.checkPosTag(sentenceIndex, occ):
-        #     if at.checkDeterminer(sentenceIndex, tokenBegin):
-        #         startChunk = oriText[0:offsetBegin-2]
-        #         endChunk = oriText[offsetEnd:]
-        #         placeholder = '<DET> <OCC>'
-        #         template = startChunk + placeholder + endChunk
-        #     else:
-        #         startChunk = oriText[0:offsetBegin]
-        #         endChunk = oriText[offsetEnd:]
-        #         placeholder = '<OCC>'
-        #         template = startChunk + placeholder + endChunk
-            # print(oriText[0:offsetBegin])
-        # print(occ)
-        # print(oriText[offsetEnd:])
-        
-        # print()
-
+print(time.time() - start)
