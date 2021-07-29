6ef601d1bda154894ba19eb2ac854102f65ffd66
Muhammad Hilmi Asyrofi <mhilmiasyrofi@gmail.com>
2021-07-29 22:26:33 +0800

    fine tune using huggingface

---
+++ codes/fine-tuning/fine_tune.py
@@ -0,0 +1,101 @@
+###
+# source
+# https://towardsdatascience.com/fine-tuning-pretrained-nlp-models-with-huggingfaces-trainer-6326a4456e7b
+#
+
+import os
+import pandas as pd
+import numpy as np
+from sklearn.metrics import accuracy_score, recall_score, precision_score, f1_score
+
+import torch
+from sklearn.model_selection import train_test_split
+from torch.utils.data import DataLoader
+from transformers import AdamW
+from transformers import AutoTokenizer, AutoModelForSequenceClassification, Trainer, TrainingArguments
+from transformers import EarlyStoppingCallback
+
+
+from utils import read_imdb_test, read_imdb_train, IMDbDataset
+
+
+def compute_metrics(p):
+    pred, labels = p
+    pred = np.argmax(pred, axis=1)
+
+    accuracy = accuracy_score(y_true=labels, y_pred=pred)
+    recall = recall_score(y_true=labels, y_pred=pred)
+    precision = precision_score(y_true=labels, y_pred=pred)
+    f1 = f1_score(y_true=labels, y_pred=pred)
+
+    return {"accuracy": accuracy, "precision": precision, "recall": recall, "f1": f1}
+
+
+if __name__ == "__main__" :
+
+    DATA_DIR = "./../../asset/imdb/"
+
+    train_texts, train_labels = read_imdb_train(DATA_DIR)
+    
+    # check_data()
+
+    train_texts, val_texts, train_labels, val_labels = train_test_split(
+        train_texts, train_labels, test_size=.1)
+
+    train_texts = list(train_texts)
+    val_texts = list(val_texts)
+    train_labels = list(train_labels)
+    val_labels = list(val_labels)
+    
+    model_name = "bert-base-cased"
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+    # check_data()
+
+    train_encodings = tokenizer(train_texts, truncation=True, padding=True, max_length = 512)
+    val_encodings = tokenizer(val_texts, truncation=True, padding=True, max_length=512)
+    
+
+    train_dataset = IMDbDataset(train_encodings, train_labels)
+    val_dataset = IMDbDataset(val_encodings, val_labels)
+    
+    model = AutoModelForSequenceClassification.from_pretrained(model_name)
+
+    training_args = TrainingArguments(
+        output_dir='./results',          # output directory
+        num_train_epochs=5,              # total number of training epochs
+        per_device_train_batch_size=8,  # batch size per device during training
+        per_device_eval_batch_size=64,   # batch size for evaluation
+        warmup_steps=500,                # number of warmup steps for learning rate scheduler
+        weight_decay=0.01,               # strength of weight decay
+        logging_dir='./logs',            # directory for storing logs
+        logging_steps=500,
+        learning_rate=5e-5,
+        seed=0,
+        evaluation_strategy="steps",
+        load_best_model_at_end=True
+    )
+
+
+    trainer = Trainer(
+        # the instantiated 🤗 Transformers model to be trained
+        model=model,
+        args=training_args,                  # training arguments, defined above
+        train_dataset=train_dataset,         # training dataset
+        eval_dataset=val_dataset,             # evaluation dataset
+        compute_metrics=compute_metrics,
+    )
+
+    # trainer = Trainer(
+    #     # the instantiated 🤗 Transformers model to be trained
+    #     model=model,
+    #     args=training_args,                  # training arguments, defined above
+    #     train_dataset=train_dataset,         # training dataset
+    #     eval_dataset=val_dataset,             # evaluation dataset
+    #     compute_metrics=compute_metrics,
+    #     callbacks=[EarlyStoppingCallback(
+    #         early_stopping_patience=10)],
+
+    # )
+
+    trainer.train()
