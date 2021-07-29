caca12b15e244b19f8427beb3e7c8b60318e14e6
Muhammad Hilmi Asyrofi <mhilmiasyrofi@gmail.com>
2021-07-29 22:26:51 +0800

    util functions

---
+++ codes/fine-tuning/utils.py
@@ -0,0 +1,35 @@
+import os
+import pandas as pd
+
+import torch
+
+def read_imdb_data(fpath):
+    data = pd.read_csv(fpath, header=None, sep="\t")
+    return data
+
+
+def read_imdb_train(data_dir):
+    fpath = os.path.join(data_dir, "train.csv")
+    df = read_imdb_data(fpath)
+    return df[1].values, df[0].values
+
+
+def read_imdb_test(data_dir):
+    fpath = os.path.join(data_dir, "test.csv")
+    df = read_imdb_data(fpath)
+    return df[1].values, df[0].values
+
+
+class IMDbDataset(torch.utils.data.Dataset):
+    def __init__(self, encodings, labels):
+        self.encodings = encodings
+        self.labels = labels
+
+    def __getitem__(self, idx):
+        item = {key: torch.tensor(val[idx])
+                for key, val in self.encodings.items()}
+        item['labels'] = torch.tensor(self.labels[idx])
+        return item
+
+    def __len__(self):
+        return len(self.labels)
