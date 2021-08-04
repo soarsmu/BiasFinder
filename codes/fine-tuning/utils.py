f0ad2dda705d34fbed80e7b492c9791b3d83d760
Muhammad Hilmi Asyrofi <mhilmiasyrofi@gmail.com>
2021-08-04 22:06:13 +0800

    helper for loading twitter dataset

--- codes/fine-tuning/utils.py
+++ codes/fine-tuning/utils.py
@@ -7,20 +7,32 @@ def read_imdb_data(fpath):
     data = pd.read_csv(fpath, header=None, sep="\t")
     return data
 
+def read_twitter_data(fpath):
+    data = pd.read_csv(fpath, header=None, sep="\t")
+    return data
 
 def read_imdb_train(data_dir):
     fpath = os.path.join(data_dir, "train.csv")
     df = read_imdb_data(fpath)
-    return df[1].values, df[0].values
-
+    return df[0].values, df[1].values
 
 def read_imdb_test(data_dir):
     fpath = os.path.join(data_dir, "test.csv")
     df = read_imdb_data(fpath)
-    return df[1].values, df[0].values
+    return df[0].values, df[1].values
+
+def read_twitter_train(data_dir):
+    fpath = os.path.join(data_dir, "train.csv")
+    df = read_twitter_data(fpath)
+    return df[0].values, df[1].values
+
+def read_twitter_test(data_dir):
+    fpath = os.path.join(data_dir, "test.csv")
+    df = read_twitter_data(fpath)
+    return df[0].values, df[1].values
 
 
-class IMDbDataset(torch.utils.data.Dataset):
+class BiasFinderDataset(torch.utils.data.Dataset):
     def __init__(self, encodings, labels):
         self.encodings = encodings
         self.labels = labels
