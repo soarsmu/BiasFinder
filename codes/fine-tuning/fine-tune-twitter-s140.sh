36839866604816ada750049205fe88610f419b73
Muhammad Hilmi Asyrofi <mhilmiasyrofi@gmail.com>
2021-08-05 10:51:56 +0800

    script for predicting twitter mutants

--- codes/fine-tuning/fine-tune-twitter-s140.sh
+++
@@ -1,1 +0,0 @@
-python fine_tune.py --task twitter_s140 --model bert-base-uncased --gpu-id gpu1 --train-bs 8
---
+++ codes/fine-tuning/predict-twitter.sh
@@ -0,0 +1,5 @@
+## genereate mutants using biasfinder on Twitter S140 dataset
+## A model which is fine-tuned using Twitter SemEval dataset is used for predicting the mutants
+python predict.py --mutation-tool biasfinder --bias-type gender --task twitter_semeval --model bert-base-uncased --mutant twitter_s140
+
+# python predict.py --mutation-tool biasfinder --bias-type gender --task twitter_semeval --model bert-base-uncased --mutant twitter_semeval
