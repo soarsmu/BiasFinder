71db1993d65420a83e4da82c645f043dd8d5d8e7
Muhammad Hilmi Asyrofi <mhilmiasyrofi@gmail.com>
2021-08-05 10:50:38 +0800

    add argument parser

--- codes/gender/biasfinder_generate_mutant.py
+++ codes/gender/biasfinder_generate_mutant.py
@@ -1,8 +1,7 @@
 import pandas as pd
 import numpy as np
-import math
-import spacy
 import os
+import argparse
 
 import time
 
@@ -12,105 +11,104 @@ from multiprocessing import Pool, Process, Queue, Manager
 import multiprocessing
 
 
-def compute_mut():
-    '''for multiprocessing uaage'''
-    while True:
-        if not q.empty():
-            label, text = q.get()
-            text = preprocessText(text)
-            mg = MutantGeneration(text)
-
-            if len(mg.getMutants()) > 0:
-                original = [text] * len(mg.getMutants())
-                label = [label] * len(mg.getMutants())
-                template = mg.getTemplates()
-                mutant = mg.getMutants()
-                gender = mg.getGenders()
-                q_to_store.put((
-                    original, label, template, mutant, gender
-                ))
-        else:
-            print("Finished")
-            return
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--task', default="imdb", type=str)
+    
+    return parser.parse_args()
 
 
-df = pd.read_csv("../../asset/imdb/test.csv", names=["label", "sentence"], sep="\t")
+def generate_mutant():
 
-# df = pd.read_csv("../../data/biasfinder/template_gender.csv")
+    
+    def compute_mut():
+        '''for multiprocessing uaage'''
+        while True:
+            if not q.empty():
+                label, text = q.get()
+                text = preprocessText(text)
+                mg = MutantGeneration(text)
 
-# df = pd.read_csv("user_study_country-artha.csv")
-# df = df[df["mod"] == 0]
-# df = df[df["is_make_sense"] == "Yes"]
+                if len(mg.getMutants()) > 0:
+                    original = [text] * len(mg.getMutants())
+                    label = [label] * len(mg.getMutants())
+                    template = mg.getTemplates()
+                    mutant = mg.getMutants()
+                    gender = mg.getGenders()
+                    q_to_store.put((
+                        original, label, template, mutant, gender
+                    ))
+            else:
+                print("Finished")
+                # return
+                break
 
-start = time.time()
+    args = get_args()
+    
+    fpath = f"../../asset/{args.task}/test.csv"
 
-originals = []
-templates = []
-mutants = []
-labels = []
-identifiers = []
-types = []
-genders = []
-countries = []
+    df = pd.read_csv(fpath, names=["label", "sentence"], sep="\t")
 
+    start = time.time()
 
-n_template = 0
+    originals = []
+    templates = []
+    mutants = []
+    labels = []
+    genders = []
+    
+    i = 0
+    
+    manager = multiprocessing.Manager()
 
-
-
-i = 0
-counter = 0
-
-manager = multiprocessing.Manager()
-
-q = manager.Queue()
-q_to_store = manager.Queue()
-
-
-for index, row in df.iterrows():
-    label = row["label"]
-    text = row["sentence"]
-
-    q.put((label, text))
-
-
-
-numList = []
-for i in range(8) :
-    p = multiprocessing.Process(target=compute_mut, args=())
-    numList.append(p)
-    p.start()
-
-for i in numList:
-    i.join()
-
-print("Generation Process finished.")
-
-while not q_to_store.empty():
-    original, label, template, mutant, gender = q_to_store.get()
-    originals.extend(original)
-    labels.extend(label)
-    templates.extend(template)
-    mutants.extend(mutant)
-    genders.extend(gender)
+    q = manager.Queue()
+    q_to_store = manager.Queue()
 
 
-end = time.time()
-print("Execution Time: ", end-start)
+    for index, row in df.iterrows():
+        label = row["label"]
+        text = row["sentence"]
 
-dm = pd.DataFrame(data={"label": labels, "mutant": mutants, "template": templates, "original": originals, "gender": genders})
+        q.put((label, text))
 
-dm = dm.drop_duplicates()
+    numList = []
+    for i in range(5) :
+        p = multiprocessing.Process(target=compute_mut, args=())
+        numList.append(p)
+        p.start()
 
-dm["template"] = dm["template"].astype("category")
-dm["template_id"] = dm["template"].cat.codes
+    for i in numList:
+        i.join()
 
-# print(n_template)
-# print(len(dm))
+    print("Generation Process finished.")
 
-data_dir = "../../data/biasfinder/gender/"
+    while not q_to_store.empty():
+        original, label, template, mutant, gender = q_to_store.get()
+        originals.extend(original)
+        labels.extend(label)
+        templates.extend(template)
+        mutants.extend(mutant)
+        genders.extend(gender)
 
-if not os.path.exists(data_dir) :
-    os.makedirs(data_dir)
 
-dm.to_csv(data_dir + "test.csv", index=None, header=None, sep="\t")
+    end = time.time()
+    print("Execution Time: ", end-start)
+
+    dm = pd.DataFrame(data={"label": labels, "mutant": mutants, "template": templates, "original": originals, "gender": genders})
+
+    dm = dm.drop_duplicates()
+
+    dm["template"] = dm["template"].astype("category")
+    dm["template_id"] = dm["template"].cat.codes
+
+
+    output_dir = f"../../data/biasfinder/gender/{args.task}/"
+
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    dm.to_csv(output_dir + "test.csv", index=None, header=None, sep="\t")
+
+
+if __name__ == "__main__" :
+    generate_mutant()
