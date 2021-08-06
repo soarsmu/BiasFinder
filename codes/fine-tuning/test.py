7a070868759933fc0fce4e40be055715b6b8fb4c
Muhammad Hilmi Asyrofi <mhilmiasyrofi@gmail.com>
2021-08-06 12:47:10 +0800

    evaluate the performance of fine-tune model on test set

--- codes/fine-tuning/test.py
+++ codes/fine-tuning/test.py
@@ -1,3 +1,11 @@
+import gc
+from utils import read_imdb_test, read_twitter_test, CustomDataset
+from torch.utils.data import DataLoader
+from pathlib import Path
+import json
+from tqdm import tqdm
+import pickle
+import argparse
 import os
 import pandas as pd
 import numpy as np
@@ -8,7 +16,6 @@ import torch
 from sklearn.model_selection import train_test_split
 from transformers import AutoTokenizer, AutoModelForSequenceClassification, Trainer, TrainingArguments
 
-from utils import read_imdb_test, IMDbDataset
 
 
 def compute_metrics(pred, labels):
@@ -19,39 +26,75 @@ def compute_metrics(pred, labels):
 
     return {"accuracy": accuracy, "precision": precision, "recall": recall, "f1": f1}
 
-if __name__ == "__main__":
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--task', default="imdb", type=str,
+                        help='dataset for fine-tuning the model')
+    parser.add_argument('--model', default='bert-base-uncased')
+    parser.add_argument('--dataset', default='imdb',
+                        help='dataset for evaluating the performance of model')
+    parser.add_argument('--batch-size', default=64, type=int)
 
-    DATA_DIR = "./../../asset/imdb/"
+    return parser.parse_args()
 
-    test_texts, test_labels = read_imdb_test(DATA_DIR)
+
+def find_best_checkpoint(checkpoint_dir):
+    """
+    Find the best checkpoint in the directory
+    :param checkpoint_dir: dir where the checkpoints are being saved.
+    :return: the checkpoint where it achieve lowest loss
+    """
+    ckpt_files = os.listdir(checkpoint_dir)  # list of strings
+    steps = [int(filename[11:])
+             for filename in ckpt_files]  # 'epoch={int}.ckpt' filename format
+    # print(steps)
+    last_step = max(steps)
+    training_state_fpath = os.path.join(
+        checkpoint_dir, f"checkpoint-{last_step}/trainer_state.json")
+
+    f = open(training_state_fpath,)  # Opening JSON file
+    data = json.load(f)  # returns JSON object as a dictionary
+
+    return data["best_model_checkpoint"]
+
+
+def predict():
+
+    args = get_args()
+
+    data_dir = f"./../../asset/{args.dataset}/"
+
+    if args.dataset == "imdb":
+        test_labels, test_texts = read_imdb_test(data_dir)
+    elif args.dataset == "twitter_semeval" or args.dataset == "twitter_s140":
+        test_labels, test_texts = read_twitter_test(data_dir)
+    else:
+        raise ValueError("Unknown dataset used for generating mutants")
+
+    # test_texts = list(test_texts)[:100]
+    # test_labels = list(test_labels)[:100]
 
     test_texts = list(test_texts)
     test_labels = list(test_labels)
 
-    # model_name = "bert-base-cased"
-    # model_name = "bert-base-uncased"
-    model_name = "roberta-base"
-    # model_name = "microsoft/deberta-large-mnli"
+    model_name = args.model
     tokenizer = AutoTokenizer.from_pretrained(model_name)
 
     test_encodings = tokenizer(
         test_texts, truncation=True, padding=True, max_length=512)
-    test_dataset = IMDbDataset(test_encodings, test_labels)
+    test_dataset = CustomDataset(test_encodings, test_labels)
 
-    if model_name == "bert-base-uncased" :
-        checkpoint_name = "./results/bert-base-uncased/gpu1/checkpoint-42000"
-        # checkpoint_name = "./results/bert-base-uncased/gpu1/checkpoint-2500"
-    elif model_name == "bert-base-cased" :
-        checkpoint_name = "./results/bert-base-cased/gpu0/checkpoint-500"
-    elif model_name == "roberta-base":
-        checkpoint_name = "./results/roberta-base/gpu1/checkpoint-4000"
-    model = AutoModelForSequenceClassification.from_pretrained(checkpoint_name)
+    checkpoint_dir = f"./models/{args.task}/{args.model}/"
+    best_checkpoint = find_best_checkpoint(checkpoint_dir)
 
-    # Define test trainer
+    model = AutoModelForSequenceClassification.from_pretrained(best_checkpoint)
+
     test_trainer = Trainer(model)
 
-    # Make prediction
-    raw_pred, _, _ = test_trainer.predict(test_dataset)
+    test_loader = DataLoader(
+        test_dataset, batch_size=args.batch_size, shuffle=False)
+    raw_pred, _, _ = test_trainer.prediction_loop(
+        test_loader, description="prediction")
 
     # Preprocess raw predictions
     y_pred = np.argmax(raw_pred, axis=1)
@@ -59,4 +102,15 @@ if __name__ == "__main__":
     metrics = compute_metrics(y_pred, test_labels)
     print(metrics)
 
+    fpath = os.path.join(data_dir, f"predictions/{args.model}.pkl")
 
+    parent_dir = "/".join(str(fpath).split('/')[:-1])
+    if not os.path.exists(parent_dir):
+        os.makedirs(parent_dir)
+
+    with open(fpath, 'wb') as f:
+        pickle.dump(y_pred, f)
+
+
+if __name__ == "__main__":
+    predict()
