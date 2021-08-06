d3cab8dc991287ce8ea1c770a8ad1d31fadd7ff7
Muhammad Hilmi Asyrofi <mhilmiasyrofi@gmail.com>
2021-08-06 16:08:20 +0800

    script for imdb

--- codes/fine-tuning/fine-tune-imdb.sh
+++ codes/fine-tuning/fine-tune-imdb.sh
@@ -2,11 +2,10 @@
 # python fine_tune.py --task imdb --model bert-base-cased --train-bs 8 --learning-rate 1e-5 --epochs 10 --warmup-steps 50 --logging-steps 50
 # python fine_tune.py --task imdb --model roberta-base --train-bs 8 --learning-rate 1e-5 --epochs 10 --warmup-steps 50 --logging-steps 50
 # python fine_tune.py --task imdb --model xlnet-base-cased --train-bs 8 --learning-rate 1e-5 --epochs 10 --warmup-steps 50 --logging-steps 50
-# python fine_tune.py --task imdb --model albert-base-v1 --train-bs 8 --learning-rate 1e-5 --epochs 10 --warmup-steps 50 --logging-steps 50
 
 python fine_tune.py --task imdb --model albert-base-v2 --train-bs 8 --learning-rate 1e-5 --epochs 10 --warmup-steps 50 --logging-steps 50
 python fine_tune.py --task imdb --model microsoft/mpnet-base --train-bs 8 --learning-rate 1e-5 --epochs 10 --warmup-steps 50 --logging-steps 50
+python fine_tune.py --task imdb --model microsoft/deberta-base --train-bs 4 --learning-rate 1e-5 --epochs 10 --warmup-steps 50 --logging-steps 50
 python fine_tune.py --task imdb --model google/electra-base-generator --train-bs 8 --learning-rate 1e-5 --epochs 10 --warmup-steps 50 --logging-steps 50
 python fine_tune.py --task imdb --model facebook/muppet-roberta-base --train-bs 8 --learning-rate 1e-5 --epochs 10 --warmup-steps 50 --logging-steps 50
-python fine_tune.py --task imdb --model microsoft/deberta-base --train-bs 4 --learning-rate 1e-5 --epochs 10 --warmup-steps 50 --logging-steps 50
 
