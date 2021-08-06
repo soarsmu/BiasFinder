639f101ae5bf4b73e691714202bc785ac7d2cb3b
Muhammad Hilmi Asyrofi <mhilmiasyrofi@gmail.com>
2021-08-06 12:45:54 +0800

    script for fine-tuning

--- codes/fine-tuning/fine-tune-imdb.sh
+++ codes/fine-tuning/fine-tune-imdb.sh
@@ -1,10 +1,12 @@
-# python fine_tune.py --task imdb --model microsoft/deberta-base --gpu-id gpu0 --train-bs 4
-# python fine_tune.py --task imdb --model roberta-base --gpu-id gpu0 --train-bs 8
-# python fine_tune.py --task imdb --model albert-base-v1 --gpu-id gpu0 --train-bs 8
-# python fine_tune.py --task imdb --model albert-base-v2 --gpu-id gpu0 --train-bs 8
+# python fine_tune.py --task imdb --model bert-base-uncased --train-bs 8 --learning-rate 1e-5 --epochs 10 --warmup-steps 50 --logging-steps 50
+# python fine_tune.py --task imdb --model bert-base-cased --train-bs 8 --learning-rate 1e-5 --epochs 10 --warmup-steps 50 --logging-steps 50
+# python fine_tune.py --task imdb --model roberta-base --train-bs 8 --learning-rate 1e-5 --epochs 10 --warmup-steps 50 --logging-steps 50
+# python fine_tune.py --task imdb --model xlnet-base-cased --train-bs 8 --learning-rate 1e-5 --epochs 10 --warmup-steps 50 --logging-steps 50
+# python fine_tune.py --task imdb --model albert-base-v1 --train-bs 8 --learning-rate 1e-5 --epochs 10 --warmup-steps 50 --logging-steps 50
 
+python fine_tune.py --task imdb --model albert-base-v2 --train-bs 8 --learning-rate 1e-5 --epochs 10 --warmup-steps 50 --logging-steps 50
+python fine_tune.py --task imdb --model microsoft/mpnet-base --train-bs 8 --learning-rate 1e-5 --epochs 10 --warmup-steps 50 --logging-steps 50
+python fine_tune.py --task imdb --model google/electra-base-generator --train-bs 8 --learning-rate 1e-5 --epochs 10 --warmup-steps 50 --logging-steps 50
+python fine_tune.py --task imdb --model facebook/muppet-roberta-base --train-bs 8 --learning-rate 1e-5 --epochs 10 --warmup-steps 50 --logging-steps 50
+python fine_tune.py --task imdb --model microsoft/deberta-base --train-bs 4 --learning-rate 1e-5 --epochs 10 --warmup-steps 50 --logging-steps 50
 
-python fine_tune.py --task imdb --model microsoft/mpnet-base --gpu-id gpu1 --train-bs 8
-python fine_tune.py --task imdb --model google/electra-base-generator --gpu-id gpu1 --train-bs 8
-python fine_tune.py --task imdb --model facebook/muppet-roberta-base --gpu-id gpu1 --train-bs 8
-python fine_tune.py --task imdb --model xlnet-base-cased --gpu-id gpu1 --train-bs 8
