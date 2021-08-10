c44107515e1181e315c23a3027ecb5cba228588d
Muhammad Hilmi Asyrofi <mhilmiasyrofi@gmail.com>
2021-08-10 14:45:44 +0800

    predict original texts

--- codes/fine-tuning/predict.py
+++ codes/fine-tuning/predict.py
@@ -12,7 +12,7 @@ from sklearn.model_selection import train_test_split
 from transformers import AutoTokenizer, AutoModelForSequenceClassification, Trainer, TrainingArguments
 from torch.utils.data import DataLoader
 
-from utils import read_imdb_test, read_twitter_test, CustomDataset
+from utils import read_imdb_test, read_twitter_test, read_test_data, read_original_data, CustomDataset
 
 
 def get_args():
@@ -22,6 +22,8 @@ def get_args():
     parser.add_argument('--task', default="imdb", type=str, help='dataset for fine-tuning the model')
     parser.add_argument('--model', default='bert-base-uncased')
     parser.add_argument('--mutant', default='imdb', help='dataset utilized to generate mutant')
+    parser.add_argument('--type', default='mutant', help='mutant | original')
+
     parser.add_argument('--batch-size', default=64, type=int)
 
     return parser.parse_args()
@@ -45,21 +47,45 @@ def find_best_checkpoint(checkpoint_dir):
     
     return data["best_model_checkpoint"]
 
-
+def generate_original_data(data_dir, mutation_tool):
+    if mutation_tool == "biasfinder" :
+        headers = ["label", "mutant", "template", "original", "gender"]
+    elif mutation_tool == "eec" :
+        headers = ["label", "mutant", "template",
+                   "original", "person", "gender", "emotion"]
+    elif mutation_tool ==  "mtnlp" :
+        headers = ["label", "mutant", "original", "mutation_type"]
+    df = pd.read_csv(data_dir + "test.csv", header=None, sep="\t",
+                     names=headers)
+    ori_df = df[["label", "original"]]
+    ori_df = ori_df.drop_duplicates()
+    ori_df.to_csv(data_dir + "original.csv",
+                   index=None, header=None, sep="\t")
 
 def predict():
     print("=== Predict ===")
     args = get_args()
     print(args)
 
-    data_dir = f"./../../data/{args.mutation_tool}/{args.bias_type}/{args.mutant}/"
-
-    if args.mutant == "imdb" :
-        test_labels, test_texts  = read_imdb_test(data_dir)
-    elif args.mutant == "twitter_semeval" or args.mutant == "twitter_s140" :
-        test_labels, test_texts = read_twitter_test(data_dir)
+    if args.bias_type !=  "" :
+        data_dir = f"./../../data/{args.mutation_tool}/{args.bias_type}/{args.mutant}/"
     else :
-        raise ValueError("Unknown dataset used for generating mutants")
+        data_dir = f"./../../data/{args.mutation_tool}/{args.mutant}/"
+
+    if args.type == "mutant" :
+        test_labels, test_texts = read_test_data(data_dir)
+        # if args.mutant == "imdb" :
+        #     test_labels, test_texts  = read_imdb_test(data_dir)
+        # elif args.mutant == "twitter_semeval" or args.mutant == "twitter_s140" :
+        #     test_labels, test_texts = read_twitter_test(data_dir)
+        # else :
+        #     raise ValueError("Unknown dataset used for generating mutants")
+    elif args.type == "original" :
+        # if not os.path.exists(data_dir + "original.csv") :
+        generate_original_data(data_dir, mutation_tool=args.mutation_tool)
+        test_labels, test_texts = read_original_data(data_dir)
+    else:
+        raise ValueError("Unknown type that needs to be tested")
 
     # test_texts = list(test_texts)[:100]
     # test_labels = list(test_labels)[:100]
@@ -89,7 +115,7 @@ def predict():
     # Preprocess raw predictions
     y_pred = np.argmax(raw_pred, axis=1)
 
-    fpath = os.path.join(data_dir, f"predictions/{args.model}.pkl")
+    fpath = os.path.join(data_dir, f"{args.type}-predictions/{args.model}.pkl")
     
     parent_dir = "/".join(str(fpath).split('/')[:-1])
     if not os.path.exists(parent_dir) :
