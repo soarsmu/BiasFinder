8aed662bc27bfff5b12ed2145953d80147807126
Muhammad Hilmi Asyrofi <mhilmiasyrofi@gmail.com>
2021-08-10 14:45:17 +0800

    read data

--- codes/fine-tuning/utils.py
+++ codes/fine-tuning/utils.py
@@ -3,32 +3,43 @@ import pandas as pd
 
 import torch
 
-def read_imdb_data(fpath):
-    data = pd.read_csv(fpath, header=None, sep="\t")
-    return data
-
-def read_twitter_data(fpath):
+def read_csv_data(fpath):
     data = pd.read_csv(fpath, header=None, sep="\t")
     return data
 
 def read_imdb_train(data_dir):
     fpath = os.path.join(data_dir, "train.csv")
-    df = read_imdb_data(fpath)
+    df = read_csv_data(fpath)
     return df[0].values, df[1].values
 
 def read_imdb_test(data_dir):
     fpath = os.path.join(data_dir, "test.csv")
-    df = read_imdb_data(fpath)
+    df = read_csv_data(fpath)
     return df[0].values, df[1].values
 
 def read_twitter_train(data_dir):
     fpath = os.path.join(data_dir, "train.csv")
-    df = read_twitter_data(fpath)
+    df = read_csv_data(fpath)
     return df[0].values, df[1].values
 
 def read_twitter_test(data_dir):
     fpath = os.path.join(data_dir, "test.csv")
-    df = read_twitter_data(fpath)
+    df = read_csv_data(fpath)
+    return df[0].values, df[1].values
+
+def read_train_data(data_dir):
+    fpath = os.path.join(data_dir, "train.csv")
+    df = read_csv_data(fpath)
+    return df[0].values, df[1].values
+
+def read_test_data(data_dir):
+    fpath = os.path.join(data_dir, "test.csv")
+    df = read_csv_data(fpath)
+    return df[0].values, df[1].values
+
+def read_original_data(data_dir) :
+    fpath = os.path.join(data_dir, "original.csv")
+    df = read_csv_data(fpath)
     return df[0].values, df[1].values
 
 
