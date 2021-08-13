f1b83309d5a2a9a7be7cf3a240b25062b3159f7d
Muhammad Hilmi Asyrofi <mhilmiasyrofi@gmail.com>
2021-08-13 22:59:01 +0800

    add type as a parameter

--- codes/fine-tuning/predict-twitter-semeval.sh
+++ codes/fine-tuning/predict-twitter-semeval.sh
@@ -1,19 +1,31 @@
 ## genereate mutants using biasfinder on Twitter SemEval dataset
 ## A model which is fine-tuned using Twitter SemEval dataset is used for predicting the mutants
-# python predict.py --mutation-tool biasfinder --bias-type gender --task twitter_semeval --model bert-base-uncased --mutant twitter_semeval
-# python predict.py --mutation-tool biasfinder --bias-type gender --task twitter_semeval --model bert-base-cased --mutant twitter_semeval
-# python predict.py --mutation-tool biasfinder --bias-type gender --task twitter_semeval --model roberta-base --mutant twitter_semeval
-python predict.py --mutation-tool biasfinder --bias-type gender --task twitter_semeval --model albert-base-v2 --mutant twitter_semeval
-python predict.py --mutation-tool biasfinder --bias-type gender --task twitter_semeval --model microsoft/mpnet-base --mutant twitter_semeval
-python predict.py --mutation-tool biasfinder --bias-type gender --task twitter_semeval --model microsoft/deberta-base --mutant twitter_semeval
-python predict.py --mutation-tool biasfinder --bias-type gender --task twitter_semeval --model facebook/muppet-roberta-base --mutant twitter_semeval
-python predict.py --mutation-tool biasfinder --bias-type gender --task twitter_semeval --model google/electra-base-generator --mutant twitter_semeval
+# python predict.py --type mutant --mutation-tool biasfinder --bias-type gender --task twitter_semeval --model bert-base-uncased --mutant twitter_semeval
+# python predict.py --type mutant --mutation-tool biasfinder --bias-type gender --task twitter_semeval --model bert-base-cased --mutant twitter_semeval
+# python predict.py --type mutant --mutation-tool biasfinder --bias-type gender --task twitter_semeval --model roberta-base --mutant twitter_semeval
+# python predict.py --type mutant --mutation-tool biasfinder --bias-type gender --task twitter_semeval --model albert-base-v2 --mutant twitter_semeval
+# python predict.py --type mutant --mutation-tool biasfinder --bias-type gender --task twitter_semeval --model xlnet-base-cased --mutant twitter_semeval
+# python predict.py --type mutant --mutation-tool biasfinder --bias-type gender --task twitter_semeval --model microsoft/mpnet-base --mutant twitter_semeval
+# python predict.py --type mutant --mutation-tool biasfinder --bias-type gender --task twitter_semeval --model microsoft/deberta-base --mutant twitter_semeval
+# python predict.py --type mutant --mutation-tool biasfinder --bias-type gender --task twitter_semeval --model facebook/muppet-roberta-base --mutant twitter_semeval
+# python predict.py --type mutant --mutation-tool biasfinder --bias-type gender --task twitter_semeval --model google/electra-base-generator --mutant twitter_semeval
+
+# python predict.py --type original --mutation-tool biasfinder --bias-type gender --task twitter_semeval --model bert-base-uncased --mutant twitter_semeval
+# python predict.py --type original --mutation-tool biasfinder --bias-type gender --task twitter_semeval --model bert-base-cased --mutant twitter_semeval
+# python predict.py --type original --mutation-tool biasfinder --bias-type gender --task twitter_semeval --model roberta-base --mutant twitter_semeval
+# python predict.py --type original --mutation-tool biasfinder --bias-type gender --task twitter_semeval --model albert-base-v2 --mutant twitter_semeval
+python predict.py --type original --mutation-tool biasfinder --bias-type gender --task twitter_semeval --model xlnet-base-cased --mutant twitter_semeval
+# python predict.py --type original --mutation-tool biasfinder --bias-type gender --task twitter_semeval --model microsoft/mpnet-base --mutant twitter_semeval
+# python predict.py --type original --mutation-tool biasfinder --bias-type gender --task twitter_semeval --model microsoft/deberta-base --mutant twitter_semeval
+# python predict.py --type original --mutation-tool biasfinder --bias-type gender --task twitter_semeval --model facebook/muppet-roberta-base --mutant twitter_semeval
+# python predict.py --type original --mutation-tool biasfinder --bias-type gender --task twitter_semeval --model google/electra-base-generator --mutant twitter_semeval
+
 
 # python predict.py --mutation-tool eec --bias-type gender --task twitter_semeval --model bert-base-uncased --mutant twitter_semeval
 # python predict.py --mutation-tool eec --bias-type gender --task twitter_semeval --model bert-base-cased --mutant twitter_semeval
 # python predict.py --mutation-tool eec --bias-type gender --task twitter_semeval --model roberta-base --mutant twitter_semeval
-python predict.py --mutation-tool eec --bias-type gender --task twitter_semeval --model albert-base-v2 --mutant twitter_semeval
-python predict.py --mutation-tool eec --bias-type gender --task twitter_semeval --model microsoft/mpnet-base --mutant twitter_semeval
-python predict.py --mutation-tool eec --bias-type gender --task twitter_semeval --model microsoft/deberta-base --mutant twitter_semeval
-python predict.py --mutation-tool eec --bias-type gender --task twitter_semeval --model facebook/muppet-roberta-base --mutant twitter_semeval
-python predict.py --mutation-tool eec --bias-type gender --task twitter_semeval --model google/electra-base-generator --mutant twitter_semeval
+# python predict.py --mutation-tool eec --bias-type gender --task twitter_semeval --model albert-base-v2 --mutant twitter_semeval
+# python predict.py --mutation-tool eec --bias-type gender --task twitter_semeval --model microsoft/mpnet-base --mutant twitter_semeval
+# python predict.py --mutation-tool eec --bias-type gender --task twitter_semeval --model microsoft/deberta-base --mutant twitter_semeval
+# python predict.py --mutation-tool eec --bias-type gender --task twitter_semeval --model facebook/muppet-roberta-base --mutant twitter_semeval
+# python predict.py --mutation-tool eec --bias-type gender --task twitter_semeval --model google/electra-base-generator --mutant twitter_semeval
