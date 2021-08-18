154a7fe9db687fda69ba41e1670a2bee36ce93cc
Muhammad Hilmi Asyrofi <mhilmiasyrofi@gmail.com>
2021-08-18 14:19:48 +0800

    create variables

--- codes/fine-tuning/fine-tune-twitter-s140.sh
+++ codes/fine-tuning/fine-tune-twitter-s140.sh
@@ -1,10 +1,14 @@
-# python fine_tune.py --task twitter_s140 --test-size 0.3 --model bert-base-uncased --epochs 20 --learning-rate 1e-5 --train-bs 8 --warmup-steps 50 --logging-steps 50
-# python fine_tune.py --task twitter_s140 --test-size 0.3 --model bert-base-cased --epochs 20 --learning-rate 1e-5 --train-bs 8 --warmup-steps 50 --logging-steps 50
-# python fine_tune.py --task twitter_s140 --test-size 0.3 --model roberta-base --epochs 20 --learning-rate 1e-5 --train-bs 8 --warmup-steps 50 --logging-steps 50
-# python fine_tune.py --task twitter_s140 --test-size 0.3 --model xlnet-base-cased --epochs 20 --learning-rate 1e-5 --train-bs 8 --warmup-steps 50 --logging-steps 50
 
-# python fine_tune.py --task twitter_s140 --test-size 0.2 --model albert-base-v2 --epochs 20 --learning-rate 1e-5 --train-bs 8 --warmup-steps 50 --logging-steps 50
-# python fine_tune.py --task twitter_s140 --test-size 0.2 --model microsoft/mpnet-base --epochs 20 --learning-rate 1e-5 --train-bs 8 --warmup-steps 50 --logging-steps 50
-# python fine_tune.py --task twitter_s140 --test-size 0.2 --model microsoft/deberta-base --epochs 20 --learning-rate 1e-5 --train-bs 8 --warmup-steps 50 --logging-steps 50
-# python fine_tune.py --task twitter_s140 --test-size 0.2 --model facebook/muppet-roberta-base --epochs 20 --learning-rate 1e-5 --train-bs 8 --warmup-steps 50 --logging-steps 50
-# python fine_tune.py --task twitter_s140 --test-size 0.2 --model google/electra-base-generator --epochs 20 --learning-rate 1e-5 --train-bs 8 --warmup-steps 50 --logging-steps 50
+declare -a models=("bert-base-uncased" "bert-base-cased" "roberta-base" "xlnet-base-cased" "albert-base-v2" "microsoft/mpnet-base" "google/electra-base-generator" "facebook/muppet-roberta-base" "microsoft/deberta-base")
+
+for model in ${models[@]}; do
+    python fine_tune.py \
+        --task twitter_s140 \
+        --test-size 0.02 \
+        --model bert-base-uncased \
+        --epochs 30 \
+        --learning-rate 1e-5 \
+        --train-bs 32 \
+        --warmup-steps 100 \
+        --logging-steps 100
+done
