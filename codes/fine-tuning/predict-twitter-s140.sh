9d6d19597f57f3fe35617780a4ebd4674762bbeb
Muhammad Hilmi Asyrofi <mhilmiasyrofi@gmail.com>
2021-08-22 18:01:20 +0800

    update script

--- codes/fine-tuning/predict-twitter-s140.sh
+++ codes/fine-tuning/predict-twitter-s140.sh
@@ -1,31 +1,91 @@
 ## genereate mutants using biasfinder on Twitter S140 dataset
 ## A model which is fine-tuned using Twitter S140 dataset is used for predicting the mutants
-# python predict.py --type mutant --mutation-tool biasfinder --bias-type gender --task twitter_s140 --model bert-base-uncased --mutant twitter_s140
-# python predict.py --type mutant --mutation-tool biasfinder --bias-type gender --task twitter_s140 --model bert-base-cased --mutant twitter_s140
-# python predict.py --type mutant --mutation-tool biasfinder --bias-type gender --task twitter_s140 --model roberta-base --mutant twitter_s140
-# python predict.py --type mutant --mutation-tool biasfinder --bias-type gender --task twitter_s140 --model albert-base-v2 --mutant twitter_s140
-python predict.py --type mutant --mutation-tool biasfinder --bias-type gender --task twitter_s140 --model xlnet-base-cased --mutant twitter_s140
-# python predict.py --type mutant --mutation-tool biasfinder --bias-type gender --task twitter_s140 --model microsoft/mpnet-base --mutant twitter_s140
-# python predict.py --type mutant --mutation-tool biasfinder --bias-type gender --task twitter_s140 --model microsoft/deberta-base --mutant twitter_s140
-# python predict.py --type mutant --mutation-tool biasfinder --bias-type gender --task twitter_s140 --model facebook/muppet-roberta-base --mutant twitter_s140
-# python predict.py --type mutant --mutation-tool biasfinder --bias-type gender --task twitter_s140 --model google/electra-base-generator --mutant twitter_s140
 
-# python predict.py --type original --mutation-tool biasfinder --bias-type gender --task twitter_s140 --model bert-base-uncased --mutant twitter_s140
-# python predict.py --type original --mutation-tool biasfinder --bias-type gender --task twitter_s140 --model bert-base-cased --mutant twitter_s140
-# python predict.py --type original --mutation-tool biasfinder --bias-type gender --task twitter_s140 --model roberta-base --mutant twitter_s140
-# python predict.py --type original --mutation-tool biasfinder --bias-type gender --task twitter_s140 --model albert-base-v2 --mutant twitter_s140
-python predict.py --type original --mutation-tool biasfinder --bias-type gender --task twitter_s140 --model xlnet-base-cased --mutant twitter_s140
-# python predict.py --type original --mutation-tool biasfinder --bias-type gender --task twitter_s140 --model microsoft/mpnet-base --mutant twitter_s140
-# python predict.py --type original --mutation-tool biasfinder --bias-type gender --task twitter_s140 --model microsoft/deberta-base --mutant twitter_s140
-# python predict.py --type original --mutation-tool biasfinder --bias-type gender --task twitter_s140 --model facebook/muppet-roberta-base --mutant twitter_s140
-# python predict.py --type original --mutation-tool biasfinder --bias-type gender --task twitter_s140 --model google/electra-base-generator --mutant twitter_s140
+# declare -a models=("bert-base-uncased" "bert-base-cased" "roberta-base" "xlnet-base-cased" "albert-base-v2" "microsoft/mpnet-base" "google/electra-base-generator" "facebook/muppet-roberta-base")
+# # declare -a models=("bert-base-uncased" "bert-base-cased" "xlnet-base-cased" "albert-base-v2" "microsoft/mpnet-base" "google/electra-base-generator")
+
+# for model in ${models[@]}; do
+#     python predict.py \
+#         --type mutant \
+#         --mutation-tool biasfinder \
+#         --bias-type gender \
+#         --task twitter_s140 \
+#         --model bert-base-uncased \
+#         --mutant twitter_s140
+# done
+
+# declare -a models=("bert-base-uncased" "bert-base-cased" "roberta-base" "xlnet-base-cased" "albert-base-v2" "microsoft/mpnet-base" "google/electra-base-generator" "facebook/muppet-roberta-base")
+# for model in ${models[@]}; do
+#     python predict.py \
+#         --type original \
+#         --mutation-tool biasfinder \
+#         --bias-type gender \
+#         --task twitter_s140 \
+#         --model bert-base-uncased \
+#         --mutant twitter_s140
+# done
+
+# declare -a models=("bert-base-uncased" "bert-base-cased" "roberta-base" "xlnet-base-cased" "albert-base-v2" "microsoft/mpnet-base" "google/electra-base-generator" "facebook/muppet-roberta-base")
+# for model in ${models[@]}; do
+#     python predict.py \
+#         --type mutant \
+#         --mutation-tool eec \
+#         --bias-type gender \
+#         --task twitter_s140 \
+#         --model bert-base-uncased \
+#         --mutant twitter_s140
+# done
 
 
-# python predict.py --mutation-tool eec --bias-type gender --task twitter_s140 --model bert-base-uncased --mutant twitter_s140
-# python predict.py --mutation-tool eec --bias-type gender --task twitter_s140 --model bert-base-cased --mutant twitter_s140
-# python predict.py --mutation-tool eec --bias-type gender --task twitter_s140 --model roberta-base --mutant twitter_s140
-# python predict.py --mutation-tool eec --bias-type gender --task twitter_s140 --model albert-base-v2 --mutant twitter_s140
-# python predict.py --mutation-tool eec --bias-type gender --task twitter_s140 --model microsoft/mpnet-base --mutant twitter_s140
-# python predict.py --mutation-tool eec --bias-type gender --task twitter_s140 --model microsoft/deberta-base --mutant twitter_s140
-# python predict.py --mutation-tool eec --bias-type gender --task twitter_s140 --model facebook/muppet-roberta-base --mutant twitter_s140
-# python predict.py --mutation-tool eec --bias-type gender --task twitter_s140 --model google/electra-base-generator --mutant twitter_s140
+# declare -a models=("bert-base-uncased" "bert-base-cased" "roberta-base" "xlnet-base-cased" "albert-base-v2" "microsoft/mpnet-base" "google/electra-base-generator" "facebook/muppet-roberta-base" "microsoft/deberta-base")
+declare -a models=("roberta-base" "facebook/muppet-roberta-base")
+
+for model in ${models[@]}; do
+    python predict.py \
+        --type mutant \
+        --mutation-tool biasfinder \
+        --bias-type country \
+        --task twitter_s140 \
+        --model bert-base-uncased \
+        --mutant twitter_s140
+done
+
+# # declare -a models=("bert-base-uncased" "bert-base-cased" "roberta-base" "xlnet-base-cased" "albert-base-v2" "microsoft/mpnet-base" "google/electra-base-generator" "facebook/muppet-roberta-base" "microsoft/deberta-base")
+declare -a models=("roberta-base" "facebook/muppet-roberta-base")
+for model in ${models[@]}; do
+    python predict.py \
+        --type original \
+        --mutation-tool biasfinder \
+        --bias-type country \
+        --task twitter_s140 \
+        --model bert-base-uncased \
+        --mutant twitter_s140
+done
+
+
+# # declare -a models=("bert-base-uncased" "bert-base-cased" "roberta-base" "xlnet-base-cased" "albert-base-v2" "microsoft/mpnet-base" "google/electra-base-generator" "facebook/muppet-roberta-base" "microsoft/deberta-base")
+# declare -a models=("bert-base-uncased" "bert-base-cased" "xlnet-base-cased" "albert-base-v2" "microsoft/mpnet-base" "google/electra-base-generator")
+declare -a models=("roberta-base" "facebook/muppet-roberta-base")
+
+for model in ${models[@]}; do
+    python predict.py \
+        --type mutant \
+        --mutation-tool biasfinder \
+        --bias-type occupation \
+        --task twitter_s140 \
+        --model bert-base-uncased \
+        --mutant twitter_s140
+done
+
+# # declare -a models=("bert-base-uncased" "bert-base-cased" "roberta-base" "xlnet-base-cased" "albert-base-v2" "microsoft/mpnet-base" "google/electra-base-generator" "facebook/muppet-roberta-base" "microsoft/deberta-base")
+declare -a models=("roberta-base" "facebook/muppet-roberta-base")
+
+for model in ${models[@]}; do
+    python predict.py \
+        --type original \
+        --mutation-tool biasfinder \
+        --bias-type occupation \
+        --task twitter_s140 \
+        --model bert-base-uncased \
+        --mutant twitter_s140
+done
