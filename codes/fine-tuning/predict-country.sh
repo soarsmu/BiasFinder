eca54025b0ab16f0c4e9a5676f714f6f3676c783
Muhammad Hilmi Asyrofi <mhilmiasyrofi@gmail.com>
2021-11-02 14:45:53 +0800

    predict imdb

--- codes/fine-tuning/predict-country.sh
+++ codes/fine-tuning/predict-country.sh
@@ -4,54 +4,49 @@
 
 declare -a models=("bert-base-uncased" "bert-base-cased" "roberta-base" "xlnet-base-cased" "albert-base-v2" "microsoft/mpnet-base" "google/electra-base-generator" "facebook/muppet-roberta-base")
 
-# for model in ${models[@]}; do
-#     python predict.py \
-#             --batch-size 64 \
-#             --type mutant \
-#             --mutation-tool biasfinder \
-#             --bias-type country \
-#             --task imdb \
-#             --model $model \
-#             --mutant imdb
-# done
+for model in ${models[@]}; do
+    python predict.py \
+            --batch-size 64 \
+            --type mutant \
+            --mutation-tool biasfinder \
+            --bias-type country \
+            --task imdb \
+            --model $model \
+            --mutant imdb
+done
 
-# for model in ${models[@]}; do
-#     python predict.py \
-#             --batch-size 64 \
-#             --type original \
-#             --mutation-tool biasfinder \
-#             --bias-type country \
-#             --task imdb \
-#             --model $model \
-#             --mutant imdb
-# done
+for model in ${models[@]}; do
+    python predict.py \
+            --batch-size 64 \
+            --type original \
+            --mutation-tool biasfinder \
+            --bias-type country \
+            --task imdb \
+            --model $model \
+            --mutant imdb
+done
 
-# for model in ${models[@]}; do
-#     python predict.py \
-#             --batch-size 64 \
-#             --type mutant \
-#             --mutation-tool biasfinder \
-#             --bias-type country \
-#             --task twitter_s140 \
-#             --model $model \
-#             --mutant twitter_s140
-# done
+for model in ${models[@]}; do
+    python predict.py \
+            --batch-size 64 \
+            --type mutant \
+            --mutation-tool biasfinder \
+            --bias-type country \
+            --task twitter_s140 \
+            --model $model \
+            --mutant twitter_s140
+done
 
-# for model in ${models[@]}; do
-#     python predict.py \
-#             --batch-size 64 \
-#             --type original \
-#             --mutation-tool biasfinder \
-#             --bias-type country \
-#             --task twitter_s140 \
-#             --model $model \
-#             --mutant twitter_s140
-# done
-
-
-
-
-
+for model in ${models[@]}; do
+    python predict.py \
+            --batch-size 64 \
+            --type original \
+            --mutation-tool biasfinder \
+            --bias-type country \
+            --task twitter_s140 \
+            --model $model \
+            --mutant twitter_s140
+done
 
 
 for model in ${models[@]}; do
@@ -65,36 +60,36 @@ for model in ${models[@]}; do
             --mutant imdb
 done
 
-# for model in ${models[@]}; do
-#     python predict.py \
-#             --batch-size 64 \
-#             --type original \
-#             --mutation-tool mtnlp \
-#             --bias-type country \
-#             --task imdb \
-#             --model $model \
-#             --mutant imdb
-# done
+for model in ${models[@]}; do
+    python predict.py \
+            --batch-size 64 \
+            --type original \
+            --mutation-tool mtnlp \
+            --bias-type country \
+            --task imdb \
+            --model $model \
+            --mutant imdb
+done
 
-# for model in ${models[@]}; do
-#     python predict.py \
-#             --batch-size 64 \
-#             --type mutant \
-#             --mutation-tool mtnlp \
-#             --bias-type country \
-#             --task twitter_s140 \
-#             --model $model \
-#             --mutant twitter_s140
-# done
+for model in ${models[@]}; do
+    python predict.py \
+            --batch-size 64 \
+            --type mutant \
+            --mutation-tool mtnlp \
+            --bias-type country \
+            --task twitter_s140 \
+            --model $model \
+            --mutant twitter_s140
+done
 
-# for model in ${models[@]}; do
-#     python predict.py \
-#             --batch-size 64 \
-#             --type original \
-#             --mutation-tool mtnlp \
-#             --bias-type country \
-#             --task twitter_s140 \
-#             --model $model \
-#             --mutant twitter_s140
-# done
+for model in ${models[@]}; do
+    python predict.py \
+            --batch-size 64 \
+            --type original \
+            --mutation-tool mtnlp \
+            --bias-type country \
+            --task twitter_s140 \
+            --model $model \
+            --mutant twitter_s140
+done
 
--- codes/fine-tuning/predict-imdb-occupation.sh
+++ codes/fine-tuning/predict-imdb-occupation.sh
@@ -1,23 +1,19 @@
-## genereate mutants using biasfinder on IMDB test set
-## A model which is fine-tuned using IMDB train set 
-## This model is used for predicting the mutants
-python predict.py --type mutant --mutation-tool biasfinder --bias-type occupation --task imdb --model bert-base-uncased --mutant imdb
-# python predict.py --type mutant --mutation-tool biasfinder --bias-type occupation --task imdb --model bert-base-cased --mutant imdb
-# python predict.py --type mutant --mutation-tool biasfinder --bias-type occupation --task imdb --model roberta-base --mutant imdb
-# python predict.py --type mutant --mutation-tool biasfinder --bias-type occupation --task imdb --model xlnet-base-cased --mutant imdb
-# python predict.py --type mutant --mutation-tool biasfinder --bias-type occupation --task imdb --model albert-base-v2 --mutant imdb
-# python predict.py --type mutant --mutation-tool biasfinder --bias-type occupation --task imdb --model microsoft/mpnet-base --mutant imdb
-# python predict.py --type mutant --mutation-tool biasfinder --bias-type occupation --task imdb --model google/electra-base-generator --mutant imdb
-# python predict.py --type mutant --mutation-tool biasfinder --bias-type occupation --task imdb --model facebook/muppet-roberta-base --mutant imdb
-# python predict.py --type mutant --mutation-tool biasfinder --bias-type occupation --task imdb --model microsoft/deberta-base --mutant imdb
+python predict.py --batch-size 32 --type original --mutation-tool biasfinder --bias-type occupation --task imdb --model bert-base-uncased --mutant imdb
+python predict.py --batch-size 32 --type original --mutation-tool biasfinder --bias-type occupation --task imdb --model bert-base-cased --mutant imdb
+python predict.py --batch-size 32 --type original --mutation-tool biasfinder --bias-type occupation --task imdb --model roberta-base --mutant imdb
+python predict.py --batch-size 32 --type original --mutation-tool biasfinder --bias-type occupation --task imdb --model xlnet-base-cased --mutant imdb
+python predict.py --batch-size 32 --type original --mutation-tool biasfinder --bias-type occupation --task imdb --model albert-base-v2 --mutant imdb
+python predict.py --batch-size 32 --type original --mutation-tool biasfinder --bias-type occupation --task imdb --model microsoft/mpnet-base --mutant imdb
+python predict.py --batch-size 32 --type original --mutation-tool biasfinder --bias-type occupation --task imdb --model google/electra-base-generator --mutant imdb
+python predict.py --batch-size 32 --type original --mutation-tool biasfinder --bias-type occupation --task imdb --model facebook/muppet-roberta-base --mutant imdb
+
+python predict.py --batch-size 32 --type mutant --mutation-tool biasfinder --bias-type occupation --task imdb --model bert-base-uncased --mutant imdb
+python predict.py --batch-size 32 --type mutant --mutation-tool biasfinder --bias-type occupation --task imdb --model bert-base-cased --mutant imdb
+python predict.py --batch-size 32 --type mutant --mutation-tool biasfinder --bias-type occupation --task imdb --model roberta-base --mutant imdb
+python predict.py --batch-size 32 --type mutant --mutation-tool biasfinder --bias-type occupation --task imdb --model xlnet-base-cased --mutant imdb
+python predict.py --batch-size 32 --type mutant --mutation-tool biasfinder --bias-type occupation --task imdb --model albert-base-v2 --mutant imdb
+python predict.py --batch-size 32 --type mutant --mutation-tool biasfinder --bias-type occupation --task imdb --model microsoft/mpnet-base --mutant imdb
+python predict.py --batch-size 32 --type mutant --mutation-tool biasfinder --bias-type occupation --task imdb --model google/electra-base-generator --mutant imdb
+python predict.py --batch-size 32 --type mutant --mutation-tool biasfinder --bias-type occupation --task imdb --model facebook/muppet-roberta-base --mutant imdb
 
 
-python predict.py --type original --mutation-tool biasfinder --bias-type occupation --task imdb --model bert-base-uncased --mutant imdb
-# python predict.py --type original --mutation-tool biasfinder --bias-type occupation --task imdb --model bert-base-cased --mutant imdb
-# python predict.py --type original --mutation-tool biasfinder --bias-type occupation --task imdb --model roberta-base --mutant imdb
-# python predict.py --type original --mutation-tool biasfinder --bias-type occupation --task imdb --model xlnet-base-cased --mutant imdb
-# python predict.py --type original --mutation-tool biasfinder --bias-type occupation --task imdb --model albert-base-v2 --mutant imdb
-# python predict.py --type original --mutation-tool biasfinder --bias-type occupation --task imdb --model microsoft/mpnet-base --mutant imdb
-# python predict.py --type original --mutation-tool biasfinder --bias-type occupation --task imdb --model google/electra-base-generator --mutant imdb
-# python predict.py --type original --mutation-tool biasfinder --bias-type occupation --task imdb --model facebook/muppet-roberta-base --mutant imdb
-# python predict.py --type original --mutation-tool biasfinder --bias-type occupation --task imdb --model microsoft/deberta-base --mutant imdb
